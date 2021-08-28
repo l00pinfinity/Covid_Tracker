ba58bbabea09c1ca2a050a0ae76a0454a02531a4
loop <loopinfinity@criptext.com>
2021-08-28 13:07:23 +0300

    hide api keys

--- app/src/main/java/co/ke/snilloc/covid_19tracking/Constants.java
+++ app/src/main/java/co/ke/snilloc/covid_19tracking/Constants.java
@@ -1,4 +1,6 @@
 package co.ke.snilloc.covid_19tracking;
 
 public class Constants {
+    public static final String BASE_URL = "";
+    public static final String COVID_TRACKER_API_KEY = BuildConfig.COVID_TRACKER_API_KEY;
 }
