d1f2ea471f11ea2e1d77d4a4ed09925753aab872
loop <loopinfinity@criptext.com>
2021-09-14 17:25:01 +0300

    update global total cases

--- app/src/main/java/co/ke/snilloc/covid_19tracking/ui/TrackingActivity.java
+++ app/src/main/java/co/ke/snilloc/covid_19tracking/ui/TrackingActivity.java
@@ -11,6 +11,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import co.ke.snilloc.covid_19tracking.R;
+import co.ke.snilloc.covid_19tracking.models.Tracker;
+import co.ke.snilloc.covid_19tracking.network.CovidTrackerClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TrackingActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
@@ -24,10 +29,27 @@ public class TrackingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tracking);
         ButterKnife.bind(this);
 
+        fetchWorldData();
         mSeeAllButton.setOnClickListener(v -> {
             Intent intent = new Intent(TrackingActivity.this,WorldActivity.class);
             startActivity(intent);
         });
 
+    }
+
+    private void fetchWorldData() {
+        CovidTrackerClient.getClient().getWorldCovidData().enqueue(new Callback<Tracker>() {
+            @Override
+            public void onResponse(Call<Tracker> call, Response<Tracker> response) {
+                if (response.isSuccessful()){
+                    assert response.body() != null;
+                    mGlobalCases.setText(response.body().getTotalCasesText());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Tracker> call, Throwable t) {
+            }
+        });
     }
 }
